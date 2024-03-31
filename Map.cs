3ace17239f4aa567f851cd026aeb791912d7cd8f
cameron-astor <cameron.astor@gmail.com>
2024-03-31 00:18:26 -0700

    hex grid basic implementation

--- Map.cs
+++ Map.cs
@@ -10,11 +10,16 @@ public partial class Map : Node2D
 
 	// We are storing the map with axial coordinates (cube
 	// coordinate s may be derived: s = -q-r).
-	private Dictionary<Vector2I, Tile> map;
+	private Tile[,] map;
+	// private Dictionary<Vector2I, Tile> map;
 
 	// Dimensions of the map (in tiles)
 	private int WIDTH = 10;
 	private int HEIGHT = 10;
+
+	// Radius of the outer circle of the hexagon tiles.
+	// Decide once for the game and should not change.
+	private const int TILE_SIZE = 144;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -22,9 +27,27 @@ public partial class Map : Node2D
 		// Load packed scene
 		this.hexTileScene = ResourceLoader.Load<PackedScene>("res://Tile.tscn");
 
-		this.map = new Dictionary<Vector2I, Tile>();
+		this.map = new Tile[WIDTH, HEIGHT];
+		// this.map = new Dictionary<Vector2I, Tile>();
 
 		// test populate map
+		for (int q = 0; q < WIDTH; q++) {
+			for (int r = 0; r < HEIGHT; r++)
+			{
+				Tile t = hexTileScene.Instantiate() as Tile;
+
+				// Calculate pixel coordinates of hex
+				var pixelCoords = this.HexToPixel(new Vector2(q, r));
+
+				AddChild(t);
+
+				t.Position += pixelCoords;
+				t.SetColor(this.GetRandomColor());
+
+				this.map[q, r] = t;
+			}
+		}
+
 		
 	}
 
@@ -32,5 +55,17 @@ public partial class Map : Node2D
 	public override void _Process(double delta)
 	{
 
+	}
+
+	private Vector2 HexToPixel(Vector2 coords) {
+		var x = TILE_SIZE * (Math.Sqrt(3) * coords.X + Math.Sqrt(3)/2 * coords.Y);
+		var y = TILE_SIZE * (coords.Y * 3.0/2);
+		return new Vector2((float) x, (float) y);
+	}
+
+	private Color GetRandomColor()
+	{
+		Random r = new Random();
+		return new Color(r.Next(255)/255.0f, r.Next(255)/255.0f, r.Next(255)/255.0f);
 	}
 }
--- Tile.cs
+++ Tile.cs
@@ -3,13 +3,22 @@ using System;
 
 public partial class Tile : Node2D
 {
+
+	private Polygon2D hexagon;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		hexagon = GetNode<Polygon2D>("Hexagon");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void SetColor(Color c)
+	{
+		hexagon.Color = c;
 	}
 }
