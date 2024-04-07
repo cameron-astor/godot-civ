85c6ee41231d713c5fcc3f0c5661a061a74fd0b3
cameron-astor <cameron.astor@gmail.com>
2024-04-07 09:58:42 -0700

    basic terrain gen with perlin noise

--- HexTileMap.cs
+++ HexTileMap.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public enum TerrainType { PLAINS, WATER, DESERT, MOUNTAIN }
 
@@ -9,10 +10,9 @@ public class Hex
 
 	public TerrainType terrainType;
 	
-	public Hex(TerrainType t)
+	public Hex()
 	{
 
-		this.terrainType = t;
 	}
 }
 
@@ -26,7 +26,10 @@ public partial class HexTileMap : TileMap
 
 	Dictionary<Vector2I, Hex> mapData;
 	Dictionary<TerrainType, Vector2I> terrainTextures; // Maps terrain types to their textures in texture atlas
+	List<(float Min, float Max, TerrainType Type)> terrainGenValues;
 
+	[Export]
+	FastNoiseLite noise;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -41,18 +44,35 @@ public partial class HexTileMap : TileMap
 			{ TerrainType.MOUNTAIN, new Vector2I(1, 1) }	
 		};
 
+		List<(float Min, float Max, TerrainType Type)> terrainGenValues = new List<(float Min, float Max, TerrainType Type)>
+		{
+			(0, 0.2f, TerrainType.WATER),
+			(0.2f, 0.9f, TerrainType.PLAINS),
+			(0.9f, 1.0f, TerrainType.MOUNTAIN)
+		};
+
 		Random r = new Random();
+
+		// Perlin noise terrain gen
+		noise.Seed = r.Next(10000);
+		noise.FractalOctaves = 4;
 
 		for (int x = 0; x < width; x++)
 		{
 			for (int y = 0; y < height; y++)
 			{
 				// Create new hex
-				Hex h = new Hex((TerrainType) r.Next(4));
+				Hex h = new Hex();
+				float noiseValue = Math.Abs(noise.GetNoise2D(x, y));
+
+				// Get terrain type from Perlin noise
+				h.terrainType = terrainGenValues.First(range => noiseValue >= range.Min 
+																&& noiseValue < range.Max).Type;
+
 
 				mapData[new Vector2I(x, y)] = h;
 
-				SetCell(0, new Vector2I(x, y), 0, terrainTextures[(h.terrainType)]);
+				SetCell(0, new Vector2I(x, y), 0, terrainTextures[h.terrainType]);
 			}
 		}
 	}
