6dfec685a287eb67c8935abdb0bdc7e5126f9c85
cameron-astor <cameron.astor@gmail.com>
2024-04-25 20:03:14 -0700

    more ui

---
+++ UIManager.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+/// <summary>
+/// The UI Manager is the top level node for the user interface.
+/// It handles tasks that affect the entire UI, such as hiding and revealing windows.
+/// It takes in all UI related signals from gameplay objects, abstracting over the particular
+/// implementation and components of the UI.
+/// </summary>
+public partial class UIManager : Node
+{
+	// City UI TODO
+	Panel terrainUi;
+	Panel unitUi;
+	Panel generalUi;
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// Get UI panels
+		terrainUi = GetNode<Panel>("TerrainTileUi");
+		unitUi = GetNode<Panel>("UnitUi");
+		generalUi = GetNode<Panel>("GeneralUi");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	// Hides all UI windows that are not permanently fixed to the screen.
+	// Used when mouse clicks out of bounds, for instance.
+	public void HideAllPopups()
+	{
+		terrainUi.Visible = false;
+		unitUi.Visible = false;
+
+	}
+}
